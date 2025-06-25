e593c34fb2f8e923c153e3efe9252fcfb972c402
artyr <123@gmail.com>
2025-06-25 19:22:52 +0300

    lesson6

--- lesson6/3.py
+++ lesson6/3.py
@@ -1,6 +1,6 @@
 '''
 Запросить 3 числа. Вывести наибольшее  из них. Решить используя if.
 '''
-a, b, c = list(map(int,input().split())) or [0, 0, 0]
+a, b, c = list(map(int,input('Enter 3 num: ').split())) or [0, 0, 0]
 m = a if b <= a >= c else b if a <= b >= c else c
 print('Max:', m)
